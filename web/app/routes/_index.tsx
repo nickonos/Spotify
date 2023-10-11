c79117c64e8d75499dd16e6d9811eb094d194828
nickonos <nick.van.oijen@gmail.com>
2023-10-11 12:03:10 +0200

    change lineheight

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -10,7 +10,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div style={{ fontFamily: "system-ui, sans-serif" }}>
       <Button>Hello</Button>
     </div>
   );

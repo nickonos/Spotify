08b2a9e5cc2350f24077910521913bbeb7ab3197
nickonos <nick.van.oijen@gmail.com>
2023-10-30 14:35:16 +0100

    deploy to kubernetes

--- .github/workflow.sh
+++ .github/workflow.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # read the workflow template
-WORKFLOW_TEMPLATE=$(cat .github/services-template.yml)
+WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yml)
+SERVICE_TEMPLATE=$(cat .github/service-template.yml)
+DEPLOYMENT_TEMPLATE=$(cat .github/deployment-template.yml)
+
 
 # loop through all routes
 for ROUTE in $(find services -maxdepth 1 -mindepth 1 -type d -printf '%f\n'); do
@@ -16,5 +19,23 @@ for ROUTE in $(find services -maxdepth 1 -mindepth 1 -type d -printf '%f\n'); do
 
         # save workflow to file
         echo "${WORKFLOW}" > .github/workflows/${ROUTE}.yaml
+
+        # replace template route placeholder with route name
+        SERVICE=$(echo "${SERVICE_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
+
+        # create workflow file if it doesn't exist
+        touch .github/manifests/${ROUTE}-service.yaml
+
+        # save workflow to file
+        echo "${SERVICE}" > .github/manifests/${ROUTE}-service.yaml
+
+        # replace template route placeholder with route name
+        DEPLOYMENT=$(echo "${DEPLOYMENT_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
+
+        # create workflow file if it doesn't exist
+        touch .github/manifests/${ROUTE}-deployment.yaml
+
+        # save workflow to file
+        echo "${DEPLOYMENT}" > .github/manifests/${ROUTE}-deployment.yaml
     fi
 done

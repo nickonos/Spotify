d32fd00f6b192f6229eaee5f369106b30dfc5a67
nickonos <nick.van.oijen@gmail.com>
2024-01-23 19:07:19 +0100

    Move react project

--- web/app/api/login.server.ts
+++
@@ -1,3 +0,0 @@
-export const Login = (code: string) => {
-    fetch(process.env.GATEWAY_URL + "/api/login?code="+ code)
-}
--- web/app/api/spotify.server.ts
+++
@@ -1,5 +0,0 @@
-const SCOPE = "user-read-private user-read-email"
-
-export const GetSpotifyOAuthURL = () => {
-    return encodeURI(`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=${SCOPE}&redirect_uri=${process.env.BASE_URL + "/auth/callback"}&state=test`)
-}
